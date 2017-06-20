3fdbdb4e494a85443c29f9fdae75230a27dafe6d
Hilario Fernandes <hfernandes@pentaho.com>
2017-06-20 10:47:40 +0100

    add normalize names to compare, fix versions generator when no version exists

--- mvresources.sh
+++ mvresources.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Moves messages from src into resources, usefull when mavenizing
 # run inside the src/main directory
 
-find java -type d -name messages -print0 | \
+find java -type f -not -name *.java -print0 | \
 while IFS= read -r -d $'\0' file; do d=${file%/*}; d=${d#*/}; \
-mkdir -p resources/$d; \
-git mv "$file" "resources/$d"; \
+mkdir -p resources/$d; git mv "$file" "resources/$d"; \
 done
